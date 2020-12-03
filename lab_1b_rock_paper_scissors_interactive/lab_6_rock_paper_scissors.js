a29c87db420725e4d9a9040440368e2a075e840d
johnfial <12399190+johnfial@users.noreply.github.com>
2020-12-03 12:05:27 -0800

    lab1a update

--- lab_1b_rock_paper_scissors_interactive/lab_6_rock_paper_scissors.js
+++ lab_1b_rock_paper_scissors_interactive/lab_6_rock_paper_scissors.js
@@ -9,34 +9,14 @@
 // let user_choice = 'rock';
 
 
-// // WORKING
-// if ( computer_choice === user_choice ) {
-//     alert('You tied! Statistically very likely and as boring as expected!') }
-    
-//     else if ( user_choice === 'rock' && computer_choice === 'scissors') {
-//         alert(`Your rock smashes feeble scissors and you win!`) }
-//     else if ( user_choice === 'rock' && computer_choice === 'paper') {
-//         alert('Nobody knows why paper wraps rock, but it does. You lose!') }
-    
-//     else if ( user_choice === 'paper' && computer_choice === 'rock') { 
-//         alert('Nobody knows why paper wraps rock, but it does. You win!') }
-//     else if (user_choice === 'paper' && computer_choice === 'scissors') { 
-//         alert('You lose!') }
-
-//     else if (user_choice === 'scissors' && computer_choice === 'paper') {
-//         alert('You cut and win!') }
-//     else if ( user_choice === 'scissors' && computer_choice === 'rock') { 
-//         alert('Your scissors are broken to bits! You lose!') }        
-
-//     else { alert('Invalid entry. Refresh page to try again!')};
 
 
-let button_paper = document.getElementById("paper");
-let button_rock = document.getElementById("rock");
-let button_scissors = document.getElementById("scissors");
 
+    // PLAY() FUNCTION BEGIN
+function play(user_choice) {
+    console.log('play() function start')
 
-function play() {
+    // let user_choice = button_paper.id
     
     computer_choice = '';
     var random_number = Math.random();
@@ -47,56 +27,62 @@ function play() {
         else if ( random_number >= 0.66666666666666666666 ) {
             var computer_choice = 'scissors' }
         else { var computer_choice = 'invalid!!' };
-    console.log(`The random number is ${random_number} and computer's choice is ${computer_choice}`);
+    console.log(`random_number: ${random_number}, computer_choice: ${computer_choice}, and user_choice: ${user_choice}.`);
 
-    let li = document.createElement("Li");
-    li.innerText = `user clicked: ${button_paper.id} and computer_choice is ${computer_choice}`
-    results.appendChild(li);
-    // return something;
+    let p_text = document.createElement("p");
+    if ( computer_choice === user_choice ) {
+        p_text.innerText = `You cast ${user_choice}, and the computer chose ${computer_choice}. You tied! Statistically very likely and as boring as expected!`; }
+        // console.log('You tied! Statistically very likely and as boring as expected!')
+        
+        else if ( user_choice === 'rock' && computer_choice === 'scissors') {
+            p_text.innerText = `You cast ${user_choice}, and the computer chose ${computer_choice}. Your rock smashes feeble scissors and you win!`; }
+            // console.log(`Your rock smashes feeble scissors and you win!`) 
+
+        else if ( user_choice === 'rock' && computer_choice === 'paper') {
+            p_text.innerText = `You cast ${user_choice}, and the computer chose ${computer_choice}. Nobody knows why paper wraps rock, but it does. You lose!`; }
+            // console.log('Nobody knows why paper wraps rock, but it does. You lose!')
+        
+        else if ( user_choice === 'paper' && computer_choice === 'rock') { 
+            p_text.innerText = `You cast ${user_choice}, and the computer chose ${computer_choice}. Nobody knows why paper wraps rock, but it does. You win!`; }
+            // console.log('Nobody knows why paper wraps rock, but it does. You win!')
+
+        else if (user_choice === 'paper' && computer_choice === 'scissors') { 
+            p_text.innerText = `You cast ${user_choice}, and the computer chose ${computer_choice}. Computer cuts your paper. You lose!`; }
+            // console.log('You lose!')
+
+        else if (user_choice === 'scissors' && computer_choice === 'paper') {
+        p_text.innerText = `You cast ${user_choice}, and the computer chose ${computer_choice}. You cut the paper and win!`; }
+            // console.log('You cut the paper and win!')
+
+        else if ( user_choice === 'scissors' && computer_choice === 'rock') {
+        p_text.innerText = `You cast ${user_choice}, and the computer chose ${computer_choice}. Your scissors are broken to bits! You lose!`; }
+            // console.log('Your scissors are broken to bits! You lose!')     
+
+        else { 
+            p_text.innerText = 'Invalid entry. Refresh page to try again!'};
+
+    results.appendChild(p_text);
+        // return something? ;
 }
+    // PLAY() FUNCTION FINISH ;
 
+
+
+let button_rock = document.getElementById("rock");
+let button_paper = document.getElementById("paper");
+let button_scissors = document.getElementById("scissors");
+// let button = document.getElementsByClassName("button_user_choice");
+// console.log(`button: ${button} and button.id: ${button.id} and `); 
 
 button_rock.addEventListener('click', function(e) {
-    play()
+    play("rock");
+    // console.log(`user clicked: ${button.id} and computer_choice is ${computer_choice}`);
 });
 
 button_paper.addEventListener('click', function(e) {
-    play()
+    play("paper");
 });
 
 button_scissors.addEventListener('click', function(e) {
-    play()
+    play("scissors");
 });
-
-// EXAMPLE:
-
-
-
-// # https://github.com/PdxCodeGuild/Programming101/blob/master/labs/rps.md
-
-// import random
-
-// print('Welcome to rock, paper, scissors v1.57386.52.20200903!')
-// user_choice = input('Please select your choice of rock, paper, or scissors: ')
-
-// options = ['rock', 'paper', 'scissors']
-
-// computer_choice = random.choice(options)
-// print(f'The computer cast. . .: {computer_choice}') # firure out how to wait the text...
-
-// if user_choice == 'rock' and computer_choice == 'scissors':
-//     print('Your rock smashes feeble scissors and you win!')
-// elif user_choice == 'rock' and computer_choice == 'paper':
-//     print('Nobody knows why paper wraps rock, but it does. You lose!')
-// elif user_choice == 'paper' and computer_choice == 'rock':
-//     print('Nobody knows why paper wraps rock, but it does. You win!')
-// elif user_choice == 'paper' and computer_choice == 'scissors':
-//     print('You lose!')
-// elif user_choice == 'scissors' and computer_choice == 'paper':
-//     print('You rock and win!')
-// elif user_choice == 'scissors' and computer_choice == 'rock':
-//     print('Your scissors are broken to bits! You lose!')
-// elif user_choice == computer_choice:
-//     print('You tied! Statistically very likely but no less boring!')
-// else:
-//     print('Invalid entry!')

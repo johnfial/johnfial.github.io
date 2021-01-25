0d7a19ca5a30a74a32adcc6c3edb01f7b25df875
johnfial <12399190+johnfial@users.noreply.github.com>
2021-01-25 14:57:19 -0800

    practicing

---
+++ 2021 Coding Practice/Python Codesignal/tests.py
@@ -0,0 +1,18 @@
+def removeDuplicates(numlist):
+    return len(numlist)
+        
+
+
+list2 = [0, 0, 0, 1, 1, 3, 9, 4, 7, 7]
+
+print('#####################')
+print(removeDuplicates(list2))
+print('#####################')
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+
+# Example 2:
+
+# Input: nums = [0,0,1,1,1,2,2,3,3,4]
+# Output: 5, nums = [0,1,2,3,4]

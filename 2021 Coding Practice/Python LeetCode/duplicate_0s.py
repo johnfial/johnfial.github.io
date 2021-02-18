fc3abecc469a2e90e684a728ed98e8e7dfceb39e
johnfial <12399190+johnfial@users.noreply.github.com>
2021-02-18 12:31:37 -0800

    practice

--- 2021 Coding Practice/Python LeetCode/duplicate_0s.py
+++ 2021 Coding Practice/Python LeetCode/duplicate_0s.py
@@ -6,18 +6,30 @@
         
 
 def duplicateZeroes(arr):
-    temp_array = []
-    for num in arr:
-        if num == 0:
-            temp_array.append(0)
-            temp_array.append(0)
-        else:
-            temp_array.append(num)
-    print(arr)
-    arr = temp_array
-    print(arr)
-    return
+    # temp_array = []
+    # for num in arr:
+    #     if len(temp_array) < len(arr):
+    #         if num == 0:
+    #             temp_array.append(0)
+    #             temp_array.append(0)
+    #         else:
+    #             temp_array.append(num)
+    # print(arr)
+    # arr = temp_array
+    # print(arr)
 
-example_1 = [1,0,2,3,0,4,5,0]   # Explanation: After calling your function, the input array is modified to: [1,0,0,2,3,0,0,4]
-example_2 = [0,1,2,3] # Explanation: After calling your function, the input array is modified to: [1,2,3]
-print(duplicateZeroes(example_1))
+    i = 0
+    length = len(arr)
+    for num in range(0, len(arr)):
+        while len(arr) <= length:
+            if arr[num] == 0:
+                arr.insert(i, 0)
+                # i += 1
+            # else:
+            #     i += 1
+    print(arr)
+
+arr = [1,0,2,3,0,4,5,0]   # Explanation: After calling your function, the input array is modified to: [1,0,0,2,3,0,0,4]
+example_2 = [1,2,3] # Explanation: After calling your function, the input array is modified to: [1,2,3]
+# duplicateZeroes(example_2)
+duplicateZeroes(arr)

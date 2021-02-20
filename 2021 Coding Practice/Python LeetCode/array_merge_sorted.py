74b0a22eb0e46fb6563430c80d1750e1ec833ce0
johnfial <12399190+johnfial@users.noreply.github.com>
2021-02-19 16:15:17 -0800

    practice

--- 2021 Coding Practice/Python LeetCode/array_merge_sorted.py
+++ 2021 Coding Practice/Python LeetCode/array_merge_sorted.py
@@ -1,10 +1,7 @@
 # # https://leetcode.com/explore/learn/card/fun-with-arrays/525/inserting-items-into-an-array/3253/
 
 def merge(nums1, m, nums2, n): # nums1/2 are the arrays, m and n are the numbers initialized
-    """
-    Do not return anything, modify nums1 in-place instead.
-    """
-    c1 = 0 # counter 1
+    c1 = 0 # counter 1 for nums1
     c2 = 0 # counter 2 for nums2
     while c1 < m:
         try:
@@ -15,20 +12,34 @@ def merge(nums1, m, nums2, n): # nums1/2 are the arrays, m and n are the numbers
             pass
         c1 += 1
         c2 += 1
+    if m == 0: # i don't like this writing for only one edge case, bad philosophy...
+        print('m == 0')
+        nums1[m] = nums2[m]
+
+
 
     nums1.sort()
     print(nums1)
 
-# example 1:      
-nums1 = [1, 2, 3, 0, 0, 0]
-nums2 = [2, 5, 6]
-merge(nums1, 3, nums2, 3)
+# # example 1:      
+# nums1 = [1, 2, 3, 0, 0, 0]
+# nums2 = [2, 5, 6]
+# merge(nums1, 3, nums2, 3)
 
 # # example 2: 
 # nums1 = [1]
 # nums2 = []
 # merge(nums1, 1, nums2, 0)
 
+# # example 3:
+# nums1 = [0]
+# nums2 = [1]
+# merge(nums1, 0, nums2, 1)
+
+# example 4
+nums1 = [0,0,0,0,0]
+nums2 = [1,2,3,4,5]
+merge(nums1, 0, nums2, 5) # Expected: [1,2,3,4,5]
 
 # Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
 # The number of elements initialized in nums1 and nums2 are m and n respectively. You may assume that nums1 has a size equal to m + n such that it has enough space to hold additional elements from nums2.
